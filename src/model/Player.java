c233fe693ffd1bf0d94c43d3b7538eb0906331b0
Bryan Lachal <bryan.lachal@etu.unice.fr>
2025-10-16 12:16:06 +0200

    player.java création attributs + fonctions début

---
+++ src/model/Player.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Player {
+    private int Id_Client;
+    private String name;
+    private List<Card> CardList;
+
+
+    public Player {
+        this.Id_Client = Id_Client;
+        this.name = name;
+        setCardList(CardList);
+    }
+
+    public CreateCard (){
+
+    }
+
+    public TradeCard (){
+
+    }
+
+    public Fight(){
+
+    }
+
+}
